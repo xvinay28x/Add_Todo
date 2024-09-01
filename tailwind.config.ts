4ba79691eff94e5cdedbfc37d06b3683fe288a71
xvinay28x <xvinay28x@gmail.com>
2024-09-01 20:24:37 +0530

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,34 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#2094f3",
+          "base-100": "#ffffff",
+          "base-200": "#f7f7f7",
+          "base-300": "#e0e0e0",
+          info: "#2094f3",
+          success: "#009485",
+          warning: "#ff9900",
+          error: "#ff5724",
+        },
+      },
+      {
+        dark: {
+          primary: "#ea3367",
+          "base-100": "#202021",
+          "base-200": "#1d1e1f",
+          "base-300": "#1b1b1c",
+          info: "#2094f3",
+          success: "#009485",
+          warning: "#ff9900",
+          error: "#ff5724",
+        },
+      },
+    ],
+  },
 };
 export default config;
