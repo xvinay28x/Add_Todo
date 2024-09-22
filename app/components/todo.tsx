c461f1aa6ce74e8dce56472411ccafc3b9b1485d
xvinay28x <xvinay28x@gmail.com>
2024-09-23 00:58:42 +0530

    add checkbox

--- app/components/todo.tsx
+++ app/components/todo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { type Todo } from "@prisma/client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -10,12 +10,29 @@ import { DateTime } from "luxon";
 import { faClock } from "@fortawesome/free-solid-svg-icons/faClock";
 
 export default function TodoInfo({ todo }: { todo: Todo }): React.ReactElement {
+  const [checked, setChecked] = useState(false);
+
   return (
     <div
       key={todo?.id}
       className="flex justify-between items-center p-4 gap-2 border-b-2 border-base-300"
     >
-      <div>{todo?.title}</div>
+      <div className="flex gap-4">
+        <div>
+          <input
+            type="checkbox"
+            className="checkbox checkbox-sm rounded-full checkbox-primary"
+            onClick={() => {
+              setChecked(!checked);
+            }}
+          />
+        </div>
+        <div
+          className={`${checked ? "line-through text-stone-500" : ""} text-lg`}
+        >
+          {todo?.title}
+        </div>
+      </div>
       <div className="flex gap-4">
         <div className="flex text-stone-700 text-sm gap-2">
           <div>
